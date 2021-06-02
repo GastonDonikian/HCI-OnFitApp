6b777b90252c73c2bbe29c0767dcf1ed8f3c902c
arfernandez2000 <arfernandez@itba.ed.ar>
2021-06-02 15:43:38 -0300

    ya ni idea que cambie

---
+++ app/src/main/java/com/example/hci_onfitapp/ViewRoutineActivity.java
@@ -0,0 +1,14 @@
+package com.example.hci_onfitapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewRoutineActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_routine);
+
+    }
+}
