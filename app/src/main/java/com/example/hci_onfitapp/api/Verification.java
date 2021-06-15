d437eaa4b2306c958b3df2aac2a37764e19ea79c
arfernandez2000 <arfernandez@itba.ed.ar>
2021-06-15 07:40:24 -0300

    CASI ME OLVIDO EL VERIFICATION

---
+++ app/src/main/java/com/example/hci_onfitapp/api/Verification.java
@@ -0,0 +1,35 @@
+package com.example.hci_onfitapp.api;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Verification {
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("code")
+    @Expose
+    private String code;
+
+
+    public Verification(String email, String code) {
+        this.email = email;
+        this.code = code;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
